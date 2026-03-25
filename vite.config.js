674209bcde2ab35d8106fa7c5e76e8512d4b89f4
amirulislambd <amirulislambd313@gmail.com>
2026-03-25 18:19:29 +0600

    update repository name

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: 'https://github.com/amirulislambd/B10A7-Select-Your-XI'
-})
+  plugins: [react(), tailwindcss()],
+  base: "/B10A7-Select-Your-XI/",
+});
